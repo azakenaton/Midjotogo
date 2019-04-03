f272422d75aa2001fb83e1e2b116819df6feafbd
azakenaton <luccho7.lh@gmail.com>
2019-04-03 09:14:34 +0200

    Modification page photo avec ajout de colonne

--- views/v_stage.php
+++ views/v_stage.php
@@ -13,13 +13,34 @@ require_once(PATH_VIEWS.'parralax.php');
 <div class="main main-raised">
     <div class="container">
         <h1>Stage</h1>
-        <p>L'association Midjo Togo vous propose d'effectuer des stages conventionnés ou non pour les personnes voulant découvrir une autre culture, une autre manière de vivre.</p>
-        <iframe src="<?PATH_PDF?>stage1.pdf" width="600" height="800" id="iframe"></iframe>
+        <div class="row">
+            <div class="col-sm">
+                <p>L'association Midjo Togo vous propose d'effectuer des stages conventionnés ou non pour les personnes voulant découvrir une autre culture, une autre manière de vivre.</p></br>
+                <a href="<?PATH_PDF?>stage1.pdf" download>Téléchargement</a>
+            </div>
+            <div class="col-sm">
+                <iframe src="<?PATH_PDF?>stage1.pdf" width="450" height="600" id="iframe"></iframe>
+            </div>
+        </div>
         <h3>Nos missions</h3>
-        <iframe src="<?PATH_PDF?>stage2missions.pdf" width="600" height="800" id="iframe"></iframe>
+        <div class="row">
+            <div class="col-sm">
+                <p>Les missions proposées :</p></br>
+                <a href="<?PATH_PDF?>stage2missions.pdf" download>Téléchargement</a>
+            </div>
+            <div class="col-sm">
+                <iframe src="<?PATH_PDF?>stage2missions.pdf" width="450" height="600" id="iframe"></iframe>
+            </div>
+        </div>
         <h3>Chantier</h3>
-        <iframe src="<?PATH_PDF?>camp_chantier_ete_2019.pdf" width="600" height="800" id="iframe"></iframe>
-        <iframe src="<?PATH_PDF?>camp chantier environnement.pdf" width="600" height="800" id="iframe"></iframe>
+        <div class="row">
+            <div class="col-sm">
+                <iframe src="<?PATH_PDF?>camp_chantier_ete_2019.pdf" width="450" height="600" id="iframe"></iframe>
+            </div>
+            <div class="col-sm">
+                <iframe src="<?PATH_PDF?>camp chantier environnement.pdf" width="450" height="600" id="iframe"></iframe>
+            </div>
+        </div>
     </div>
 </div>
 
